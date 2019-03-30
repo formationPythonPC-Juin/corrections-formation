4aaf0d1e8b805e997a00ff1243ad85736d12b92a
formationPythonPC-Juin <laurent.astier@ac-limoges.fr>
2019-03-30 11:01:57 +0100

    modif de exercice2-correction

--- exercice2-correction.py
+++ exercice2-correction.py
@@ -13,15 +13,15 @@ k = pi
 #############################################
 ## question 3 : construction du tableau des x
 #############################################
-debut = 0 # début de l'ensemble des x
-fin = 5 # fin de l'ensemble des x
+debut = 0
+fin = 5
 pas  = 0.01
 x = np.arange(debut, fin+pas, pas)
 
 ###############################################
 # question 4 : construction du tableau des I(x)
 ###############################################
-# on construit le tableau I image de x par la fonction qui nous intéresse
+
 I = I0*np.cos(k*x+phi)
 
 
@@ -29,27 +29,26 @@ I = I0*np.cos(k*x+phi)
 # question 5 : construction de la courbe
 ########################################
 
-plt.plot(x,I,'b-', label="signal périodique")# on construit la courbe
+plt.plot(x,I,'b-', label="signal périodique")
 
 #####################################
 # question 6 : noms aux axes et titre
 #####################################
-plt.xlabel("axe d'espace")# nom de l'axe selon Ox
-plt.ylabel("Intensité")# de même pour l'ordonnée
-plt.title("un signal périodique")# mettre un titre
-
+plt.xlabel("axe d'espace")
+plt.ylabel("Intensité")
+plt.title("un signal périodique")
 
 ###############################
 # question 7 : limites des axes
 ###############################
 
-plt.xlim(debut, fin)# faire que le graphe ait des valeurs limites selon Ox
-plt.ylim(-I0,I0)# faire que le graphe ait des valeurs limites selon Oy
+plt.xlim(debut, fin)
+plt.ylim(-I0,I0)
 plt.grid()# on affiche une grille
 
 ###################################
 # question 8 : légende et affichage
 ###################################
 
-plt.legend()# on place la légende en utilisant le label de la courbe
-plt.show() # on affiche 
+plt.legend()
+plt.show() 
