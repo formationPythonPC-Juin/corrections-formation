1d08e813aa0cef373f963551f277dbab282753e1
formationPythonPC-Juin <laurent.astier@ac-limoges.fr>
2019-05-08 16:49:30 +0200

    ajout correction exercice 6

---
+++ exercice6-correction.py
@@ -0,0 +1,55 @@
+#############################
+# EXERCICE 6
+#############################
+
+######################################
+# question 3 : extraction des 2 listes
+######################################
+import pandas as pa
+
+table = pa.read_csv("donnees")
+
+T = table["température"]
+R = table["Réquivalente"]
+
+
+####################################################
+# question 4 : représentation graphique des 2 listes
+####################################################
+import matplotlib.pyplot as plt
+
+plt.plot(T,R,'b+', label = "points expérimentaux")
+
+plt.title("Représentation Résistance en fonction de Température")
+plt.xlabel("Température")
+plt.ylabel("Résistance")
+
+
+
+###########################
+# question 5 : modélisation
+###########################
+import numpy as np
+
+liste_coeffs = np.polyfit(T,R,1)
+
+a = liste_coeffs[0]
+b = liste_coeffs[1]
+
+
+#######################################
+# question 6 : représentation du modèle
+#######################################
+
+R_modele = a*T+b
+
+plt.plot(T,R_modele,'r-', label = "modélisation")
+
+
+
+
+
+
+
+plt.legend()
+plt.show()
